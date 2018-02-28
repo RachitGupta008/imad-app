2dc2e6810367c9ab9973382a863d0a9832ed7f88
RachitGupta008 <35948433+RachitGupta008@users.noreply.github.com>
2018-02-28 11:44:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,16 @@
 console.log('Loaded!');
+(function f(){
+    var req =  new XMLHttpRequest();
+    req.onreadystatechange = function (){
+        if(req.onreadystatechange === XMLHttpRequest.DONE){
+            if(req.status === 200){
+                var count = req.responseText;
+                var span  = document.getElementById("article1no");
+                span.innerHTML = count;
+            }
+        }
+    };
+    
+    req.open(GET,"/counter",true);
+    req.send();
+})();
