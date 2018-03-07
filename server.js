b3e49261bd3356e5f5f640d9e568779264f5427f
RachitGupta008 <35948433+RachitGupta008@users.noreply.github.com>
2018-03-07 07:38:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ app.post('/signup', jsonParser, function(req, res){
     var pass = req.body.password;
     var salt = genSalt();
     var hash = hasher(pass,salt);
-    pool.query(`INSERT INTO 'users'(username,password) VALUES ($1,$2)`, [name,hash], function (err,result){
+    pool.query(`INSERT INTO users (username,password) VALUES ($1, $2)`, [name,hash], function (err,result){
         if(err){
             res.status(500).send(err.toString());
         }
